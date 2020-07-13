03de60e325622d0107f1783932ee6b95d6619324
MDzkM <mdzkm17@gmail.com>
2020-07-13 13:41:17 +0700

    Initial commit

---
+++ blog/2020-05-19-finally-launching.md
@@ -0,0 +1,20 @@
+---
+id: finally-launching
+title: Finally Launching
+author: Muhammad Dzikra Muzaki
+author_title: Head of Design @ Webmakers ID
+author_url: https://github.com/mdzkm
+author_image_url: https://avatars1.githubusercontent.com/u/43721937
+tags: [website, launch, procrastination]
+description: The day I finally stopped procrastinating.
+---
+
+The day I finally stopped procrastinating.
+
+<!--truncate-->
+
+Yup, after almost a year having the intent of creating this website it finally launched. Honestly from my own prespective, this is just a little side project that I wanted to work on.
+
+I initially had the thought of making this when I took the Web Planning and Programming course on my faculty. It was a neat idea where I wanted to share my own personal thoughts and experiences and also learn how to design a better website, either personally or professionally.
+
+However, I didn't have a clue yet as to what content I should provide on my site. It seems too bland to just put out my name and information just for the world to see. And I haven't had enough experience yet to write a "course" or guide on programming. That kept my intent on building this website on hold until recently...
---
+++ docusaurus.config.js
@@ -0,0 +1,111 @@
+module.exports = {
+  title: 'mdzkm.',
+  tagline: 'Blog, Tutorials, and Personal Archives',
+  url: 'https://blog-mdzkm.netlify.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mdzkm', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'mdzkm.',
+      logo: {
+        href: 'https://mdzkm.wtf',
+        target: '_self'
+      },
+      links: [
+        {
+          href: 'https://resources.mdzkm.wtf',
+          label: 'Tutorials and Archives',
+          position: 'right',
+          target: '_self',
+        },
+        {
+          to: '/',
+          label: 'Blog',
+          position: 'right',
+        },
+      ],
+      // algolia: {
+      //   apiKey: 'api-key',
+      //   indexName: 'index-name',
+      // },
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Links',
+          items: [
+            {
+              label: 'About',
+              href: 'https://mdzkm.wtf/about',
+              target: '_self',
+            },
+            {
+              label: 'Projects',
+              href: 'https://mdzkm.wtf/projects',
+              target: '_self',
+            },
+            {
+              label: 'Tutorials and Archives',
+              to: 'https://resources.mdzkm.wtf',
+              target: '_self',
+            },
+            {
+              label: 'Blog',
+              to: '/',
+            },
+          ],
+        },
+        {
+          title: 'Codebase',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/mdzkm',
+            },
+            {
+              label: 'GitLab',
+              href: 'https://gitlab.com/mdzkm',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'LinkedIn',
+              href: 'https://linkedin.com/in/mdzkm/',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://instagram.com/mdzkm',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/mdzkm',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} mdzkm.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: {
+          path: './blog',
+          routeBasePath: '/', // Set this value to '/'.
+          editUrl:
+            'https://github.com/mdzkm/resources/edit/master/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
